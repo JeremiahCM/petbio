cbc045aac06f40f360d47f3db20c0d7b08657577
David Wagstaff <63911529+dwag351@users.noreply.github.com>
2022-09-29 12:23:14 +1300

    Created the required components for the

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,15 @@ import React from "react";
 //import { Route, Routes } from "react-router-dom";
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import MainLogo from './components/logo/logo';
+import Sidebar from './components/sidebar/sidebar'
+import './App.css'
 
 function App() {
   return (
-    <Box className="App">
+    <Box className="App" style={{backgroundColor: '#031e45'}}>
+      <MainLogo/>
+      <Sidebar/>
       <Typography variant="h1">
         PetBio
       </Typography>
