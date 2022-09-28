fbd07dafaf94cdb00afb3cff64f62543c9aead87
Jeremiah Martinez <itsjeremiahmartinez@gmail.com>
2022-09-28 22:53:14 +1300

    Initialized Frontend and Backend for PetBio
    Client: Frontend React App with
    - react-router-dom
    - MaterialUI
    
    Server: Backend server with
    - Express
    - NodeJS
    - Nodemon
    - MongoDB connection

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+//import { Route, Routes } from "react-router-dom";
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+function App() {
+  return (
+    <Box className="App">
+      <Typography variant="h1">
+        PetBio
+      </Typography>
+    </Box>
+  );
+}
+
+export default App;
