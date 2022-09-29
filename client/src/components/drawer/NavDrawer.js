0d2187ac3ed80b5b9b89ee931f0856521f204d34
William <wrdquinn@gmail.com>
2022-09-30 10:39:25 +1300

    draft drawer with basic stylings

--- client/src/components/drawer/NavDrawer.js
+++ client/src/components/drawer/NavDrawer.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
-import Button from '@mui/material/Button';
 import List from '@mui/material/List';
 import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
@@ -11,7 +10,7 @@ import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
 
-export default function TemporaryDrawer() {
+export default function NavDrawer() {
   const [state, setState] = React.useState({
     right: false,
   });
@@ -26,10 +25,17 @@ export default function TemporaryDrawer() {
 
   const list = (anchor) => (
     <Box
-      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+
       role="presentation"
       onClick={toggleDrawer(anchor, false)}
       onKeyDown={toggleDrawer(anchor, false)}
+      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250, 
+      bgcolor: "#031e45",
+      opacity: "20%",
+      color: "#47bfaf",
+      "text-shadow": "0px 2px 5px rgba(0,0,0,0.92)",
+      textShadowColor: "0 0 5px rgba(255,255,255,.5)",
+      marginTop: "3.5rem", }}
     >
       <List>
         {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
