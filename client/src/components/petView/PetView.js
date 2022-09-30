ceff8af3205e6117a8d083ea0e3c2f40ebce0940
Jeremiah Martinez <69489707+JeremiahCM@users.noreply.github.com>
2022-09-30 15:09:08 +1300

    Working on Pet View table

--- client/src/components/petView/PetView.js
+++ client/src/components/petView/PetView.js
@@ -5,12 +5,28 @@ import { rootShouldForwardProp } from '@mui/material/styles/styled';
 import React from 'react';
 
 const PetView = () => {
-    const [petData, setPetData] = useState('')
+    const [petData, setPetData] = useState([{ id: "12", name: "Max", breed: "Dog"}, { id: "13", name: "Oscar", breed: "Dog"}])
+
+    console.log(petData);
 
     return (
         <Grid container spacing={2} direction="column" alignItems="center" justifyContent="center">
             <Box className="pet-view">
-                <Typography>Here is text</Typography>
+                <TableContainer>
+                    <TableBody>
+                        {petData?.map((value) => (
+                            <TableRow
+                                
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            >
+                                <TableCell>ID</TableCell>
+                                <TableCell>{value.id}</TableCell>
+                                <TableCell>{value.name}</TableCell>
+                                <TableCell>{value.breed}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </TableContainer>
             </Box>
         </Grid>
     )
