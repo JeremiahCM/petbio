559ea4ab0356bf63a683edf3a81a254098f79494
David Wagstaff <63911529+dwag351@users.noreply.github.com>
2022-10-01 11:07:34 +1300

    Added graphics for the whitespace

--- client/src/components/mainpage/MainPage.js
+++ client/src/components/mainpage/MainPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 //import { Route, Routes } from "react-router-dom";
-import {Helmet} from 'react-helmet';
+import { Helmet } from "react-helmet";
 
 import Box from "@mui/material/Box";
 import { Grid } from "@mui/material";
@@ -8,6 +8,8 @@ import NavBar from "../navbar/NavBar";
 import Paper from "@mui/material/Paper";
 import "./MainPage.css";
 import TestChart from "../testchart/TestChart";
+import BackgroundImage from "../backgrounds/Background";
+import BackgroundImage2 from "../backgrounds/Background-2";
 
 const tfStyle = {
   "& .MuiOutlinedInput-root": {
@@ -26,27 +28,30 @@ function MainPage({ children }) {
   return (
     <Box className="App">
       <Helmet>
-        <title>Home |  PetBio</title>
-        </Helmet>
-        <Grid
-          container
-          spacing={0}
-          direction="column"
-          alignItems="center"
-          justifyContent="center">
-          <Navbar className="Navbar"/>
-        </Grid>
-        <Grid className="content"
-          container
-          spacing={0}
-          direction="column"
-          alignItems="center"
-          justifyContent="center"
-          sx={{bgcolor: "white", height: "60vh",color: "black"}}>
-          <Box>
-            {children}
-          </Box>
-        </Grid>
+        <title>Home | PetBio</title>
+      </Helmet>
+      <BackgroundImage />
+      <BackgroundImage2 />
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <NavBar className="Navbar" />
+      </Grid>
+      <Grid
+        className="content"
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        sx={{ bgcolor: "white", height: "100vh", color: "black" }}
+      >
+        <Box sx={{ zIndex: 1 }}>{children}</Box>
+      </Grid>
     </Box>
   );
 }
